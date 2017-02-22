692d41d1b7b4d5482c30bbf81d5eb0fca55e2029
Mohd Hafizi Bin Mohammad Nor <mohdhafizi83@gmail.com>
2017-02-22 22:21:19 +0800

    Update treeview_form.php

--- jstree/application/views/treeview_form.php
+++ jstree/application/views/treeview_form.php
@@ -5,10 +5,6 @@
     <h1>
         Hierarchy View
     </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Hierarchy</li>
-    </ol>
 </section>
 
 <div class="container-fluid">
